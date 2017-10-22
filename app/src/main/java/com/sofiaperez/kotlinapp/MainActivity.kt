da6da1913502f0a78114c6cfd6e44867f8951057
sofps <sofia.perez.salvo@gmail.com>
2017-10-22 12:46:37 +0200

    Changed layout to be able to input name and surname to modify the greeting.

--- app/src/main/java/com/sofiaperez/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/sofiaperez/kotlinapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.sofiaperez.kotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,19 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // mutable variables
-        var number = 7
-        var text: String = "testing out variables"
-        var view = View(this)
+        mainButtonTryIt.setOnClickListener {
+            if (isOk()) {
+                val person = Person(mainEditTextName.text.toString(), mainEditTextSurname.text.toString())
+                mainTextViewGreeting.text = "Hello ${person.getFullName()}!"
+            }
+        }
+    }
 
-        // immutable variables
-        val x: Int = 20
-        val y: Long = x.toLong()
-
-        val person = Person("Sofia", "Perez")
-
-        // Using kotlin extensions
-        mainTextView.text = "Hello ${person.getFullName()}!"
-
+    private fun isOk(): Boolean {
+        var isOk = true
+        if (mainEditTextName.length() == 0) {
+            mainEditTextName.error = "Name can't be empty"
+            isOk = false
+        }
+        if (mainEditTextSurname.length() == 0) {
+            mainEditTextSurname.error = "Surname can't be empty"
+            isOk = false
+        }
+        return isOk
     }
 }
