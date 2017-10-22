79106fb7cb2793b16cd5a693472b22ddd5ff4d48
sofps <sofia.perez.salvo@gmail.com>
2017-10-22 12:13:56 +0200

    Added some variables

--- app/src/main/java/com/sofiaperez/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/sofiaperez/kotlinapp/MainActivity.kt
@@ -2,11 +2,27 @@ package com.sofiaperez.kotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // mutable variables
+        var number = 7
+        var text: String = "testing out variables"
+        var view = View(this)
+
+        // immutable variables
+        val x: Int = 20
+        val y: Long = x.toLong()
+
+        val mainTextView: View = findViewById(R.id.mainTextView)
+        if (mainTextView is TextView) {
+            mainTextView.text = "Hello Sofia!"
+        }
     }
 }
