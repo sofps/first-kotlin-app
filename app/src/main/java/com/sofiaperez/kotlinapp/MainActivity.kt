a10b9c56bd9b6133c141dea9a2d58c90dce5fef5
sofps <sofia.perez.salvo@gmail.com>
2017-10-22 15:36:56 +0200

    Added data class User and played with it

--- app/src/main/java/com/sofiaperez/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/sofiaperez/kotlinapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.sofiaperez.kotlinapp
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
@@ -13,10 +16,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainButtonTryIt.setOnClickListener {
+        mainButtonTryIt.setOnClickListener { v ->
+            v.hideKeyboard()
             if (isOk()) {
                 val person = Person(mainEditTextName.text.toString(), mainEditTextSurname.text.toString())
                 mainTextViewGreeting.text = "Hello ${person.getFullName()}!"
+
+                if (!mainEditTextEmail.text.isEmpty()) {
+                    val user = User(person, mainEditTextEmail.text.toString())
+                    toast("New user with email ${user.email}")
+                }
             }
         }
 
@@ -43,5 +52,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun runLongTask(): String {
         return "Testing Anko library"
+    }
+
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/sofiaperez/kotlinapp/User.kt
@@ -0,0 +1,6 @@
+package com.sofiaperez.kotlinapp
+
+/**
+ * Created by sofiaperez on 22/10/17.
+ */
+data class User(val person: Person, val email: String)
